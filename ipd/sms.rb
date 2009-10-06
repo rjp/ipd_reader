0c834bfa8add4a776951fa76db91fa2a8c0b395b
rjp <zimpenfish@gmail.com>
2009-10-06 21:23:23 +0100

    - sms: field 11 also contains a category field for distinguishing MSC-type SMS - sms: add tag method for deciding whether it's to/from/msc whatnot - sms: remove all non-printing characters from content and to - sms: DEBUG_SMS outputs all the fields to STDERR for extra debugging

--- ipd/sms.rb
+++ ipd/sms.rb
@@ -1,16 +1,26 @@
 module IPD
 	class SMS
-	    attr_accessor :to, :date, :content, :direction
+	    attr_accessor :to, :date, :content, :direction, :other_date, :category
 
 	    def initialize()
 	        @to = nil
 	        @date = nil
+	        @other_date = nil
             @content = nil
             @direction = nil
+            @category = nil
 	    end                            
 
+        def tag
+            if @category == 5 then
+                return 'MSC'
+            else
+                return @direction == 0 ? 'To' : 'From'
+            end
+        end
+
         def to_s
-            "#{@direction == 0 ? 'To' : 'From'} #{@to} at #{@date} => ``#{@content}''"                        
+            "#{self.tag} #{@to} at #{@date}/#{other_date}\n=> ``#{@content}''"
         end                        
 	end
 
@@ -21,19 +31,25 @@ module IPD
             return @@sms
         end
         def SMSList.handle_record(f_t, f_d)
+            if ENV['DEBUG_SMS'] then
+                $stderr.puts "SMS #{f_t} #{f_d.inspect}"
+            end
+
             case f_t
             when 1
                 @@current = IPD::SMS.new
                 @@sms.push @@current
                 @@current.date = Time.at(f_d[13..20].unpack('Q')[0]/1000)
+                @@current.other_date = Time.at(f_d[21..28].unpack('Q')[0]/1000)
             when 11
                 a, b = f_d.unpack('S2')
                 @@current.direction = a
+                @@current.category = b
             when 2
-                to = f_d.unpack('a*')[0].gsub('\0', '')
+                to = f_d.unpack('a*')[0].gsub(/[^[:print:]]/, '')
                 @@current.to = to
             when 4
-                @@current.content = f_d.unpack('a*')[0]
+                @@current.content = f_d.unpack('a*')[0].gsub(/[^[:print:]]/, '')
             end
         end
     end
